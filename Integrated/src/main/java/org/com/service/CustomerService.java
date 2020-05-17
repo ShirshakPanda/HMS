36c48d6f4f0c53a0994e8f4fde1d7734ac035008
ShirshakPanda <61134954+ShirshakPanda@users.noreply.github.com>
2020-05-17 10:41:27 +0530

    Centre Frontend and Backend Done

---
+++ Integrated/src/main/java/org/com/service/CustomerService.java
@@ -0,0 +1,14 @@
+package org.com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.iter.dao.CustomerRepository;
+
+@Service("customerService")
+public class CustomerService {
+	@Autowired
+	private CustomerRepository customerdao;
+	
+	
+}
